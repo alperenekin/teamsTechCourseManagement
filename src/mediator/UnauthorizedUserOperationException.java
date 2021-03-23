065cf5ec666ac16d018eca6c114b6992a6f646df
Alperen Ekin <alperenekin97@gmail.com>
2021-03-24 01:25:29 +0300

    Exception added

--- src/mediator/UnauthorizedUserOperationException.java
+++ src/mediator/UnauthorizedUserOperationException.java
@@ -1,6 +1,11 @@
 package mediator;
 
 public class UnauthorizedUserOperationException extends Exception {
-	
+    public UnauthorizedUserOperationException() {
+        super("Product is not found in inventories!!");
+    }
+    public UnauthorizedUserOperationException(String message) {
+        super(message);
+    }
 	
 }
